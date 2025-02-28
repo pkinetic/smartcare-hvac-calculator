d8b9fb28a6b2aafb075364da1b03a50efde7ed48
pkinetic <perrykemp@gmail.com>
2025-02-28 16:56:45 -0500

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import EnergyCalculator from './components/EnergyCalculator';
+
+function App() {
+  return (
+    <div className="App">
+      <EnergyCalculator />
+    </div>
+  );
+}
+
+export default App;
